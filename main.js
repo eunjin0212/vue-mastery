9a5dcbc457ca25d9b113e8812f946a7b4a331312
eunjin0212 <eunjin9639@gmail.com>
2021-09-07 18:01:30 +0900

    Class & Style Binding

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: 'assets/images/socks_blue.jpg',
-            inventory: 0,
+            inventory: 100,
+            inStock:true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 { id: 2234, color: 'green',  image: 'assets/images/socks_green.jpg', },
